4675dc8e6e1e2d4c27fa080b1b1ddf6aa3e26e94
Moe Sial <sial.moe@gmail.com>
2016-05-03 15:40:54 -0700

    Added packet

---
+++ ParKingPacket.py
@@ -0,0 +1,58 @@
+from struct import pack
+from struct import unpack
+
+
+class ParKingPacket:
+    ENCODING = '!HLLL'
+    PADDING = 0
+    MESSAGE_TYPE_INIT = 1
+    MESSAGE_TYPE_IN = 2
+    MESSAGE_TYPE_OUT = 3
+    MESSAGE_TYPE_ALIVE = 4
+
+    def pack_init_packet(self, lot_id, capacity, vacancies):
+        """
+        packs the init packet as defined by the par king protocol
+        :param lot_id:
+        :param capacity:
+        :param vacancies:
+        :return:
+        """
+        packet = pack(self.ENCODING, self.MESSAGE_TYPE_INIT, lot_id, capacity, vacancies)
+        return packet
+
+    def pack_in_packet(self, lot_id):
+        """
+        packs the in packet as defined by the par king protocol
+        :param lot_id:
+        :return:
+        """
+        packet = pack(self.ENCODING, self.MESSAGE_TYPE_IN, lot_id, self.PADDING, self.PADDING)
+        return packet
+
+    def pack_out_packet(self, lot_id):
+        """
+        packs the out packet as defined by the par king protocol
+        :param lot_id:
+        :return:
+        """
+        packet = pack(self.ENCODING, self.MESSAGE_TYPE_OUT, lot_id, self.PADDING, self.PADDING)
+        return packet
+
+    def pack_alive_packet(self, lot_id):
+        """
+        packs the alive packet as defined by the par king protocol
+        :param lot_id:
+        :return:
+        """
+        packet = pack(self.ENCODING, self.MESSAGE_TYPE_ALIVE, lot_id, self.PADDING, self.PADDING)
+        return packet
+
+    def unpack_packet(self, packet):
+        """
+        unpacks a packet as defined by the par king protocol
+        :param packet:
+        :return: tuple (message_type, lot_id, capacity|padding, vacancies|padding)
+        """
+        packet = unpack(self.ENCODING, packet)
+        return packet
