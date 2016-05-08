cff441618d15e19ee426f9c6a3f2c4e7f55a2724
Moe Sial <sial.moe@gmail.com>
2016-05-07 22:21:51 -0700

    changed paket to have static pack and unpack methods

--- client/ParKingPacket.py
+++ client/ParKingPacket.py
@@ -2,53 +2,70 @@ from struct import pack
 from struct import unpack
 
 
-class ParKingPacket:
-    ENCODING = '!HLLL'
-    PADDING = 0
-    MESSAGE_TYPE_INIT = 1
-    MESSAGE_TYPE_IN = 2
-    MESSAGE_TYPE_OUT = 3
-    MESSAGE_TYPE_ALIVE = 4
+ENCODING = '!HLLL'
+PADDING = 0
+MESSAGE_TYPE_INIT = 1
+MESSAGE_TYPE_IN = 2
+MESSAGE_TYPE_OUT = 3
+MESSAGE_TYPE_ALIVE = 4
+MESSAGE_TYPE_RESPONSE = 5
+MESSAGE_TYPE_ERROR = 6
+
 
 def pack_init_packet(lot_id, capacity, vacancies):
     """
     packs the init packet as defined by the par king protocol
-    :param lot_id:
-    :param capacity:
-    :param vacancies:
+    :param int lot_id:
+    :param int capacity:
+    :param int vacancies:
     :return:
     """
-    packet = pack(ParKingPacket.ENCODING, ParKingPacket.MESSAGE_TYPE_INIT, lot_id, capacity, vacancies)
+    packet = pack(ENCODING, MESSAGE_TYPE_INIT, lot_id, capacity, vacancies)
     return packet
+
 
 def pack_in_packet(lot_id, z_value):
     """
     packs the in packet as defined by the par king protocol
-    :param lot_id:
-    :param z_value:
+    :param int lot_id:
+    :param int|float  z_value:
     :return:
     """
-    packet = pack(ParKingPacket.ENCODING, ParKingPacket.MESSAGE_TYPE_IN, lot_id, z_value, ParKingPacket.PADDING)
+    packet = pack(ENCODING, MESSAGE_TYPE_IN, lot_id, z_value, PADDING)
     return packet
+
 
 def pack_out_packet(lot_id, z_value):
     """
     packs the out packet as defined by the par king protocol
-    :param lot_id:
-    :param z_value
+    :param int lot_id:
+    :param int|float z_value
     :return:
     """
-    packet = pack(ParKingPacket.ENCODING, ParKingPacket.MESSAGE_TYPE_OUT, lot_id, z_value, ParKingPacket.PADDING)
+    packet = pack(ENCODING, MESSAGE_TYPE_OUT, lot_id, z_value, PADDING)
     return packet
+
 
 def pack_alive_packet(lot_id):
     """
     packs the alive packet as defined by the par king protocol
-    :param lot_id:
+    :param int lot_id:
     :return:
     """
-    packet = pack(ParKingPacket.ENCODING, ParKingPacket.MESSAGE_TYPE_ALIVE, lot_id, ParKingPacket.PADDING, ParKingPacket.PADDING)
+    packet = pack(ENCODING, MESSAGE_TYPE_ALIVE, lot_id, PADDING, PADDING)
     return packet
+
+
+def pack_response_packet(available_spaces, error_code=None):
+    """
+    packs the response packet from the server
+    :param int available_spaces:
+    :param int error_code:
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_RESPONSE, available_spaces, error_code)
+    return packet
+
 
 def unpack_packet(packet):
     """
@@ -56,5 +73,5 @@ def unpack_packet(packet):
     :param packet:
     :return: tuple (message_type, lot_id, capacity|padding, vacancies|padding)
     """
-    packet = unpack(ParKingPacket.ENCODING, packet)
+    packet = unpack(ENCODING, packet)
     return packet
--- server/ParKingPacket.py
+++ server/ParKingPacket.py
@@ -2,59 +2,76 @@ from struct import pack
 from struct import unpack
 
 
-class ParKingPacket:
-    ENCODING = '!HLLL'
-    PADDING = 0
-    MESSAGE_TYPE_INIT = 1
-    MESSAGE_TYPE_IN = 2
-    MESSAGE_TYPE_OUT = 3
-    MESSAGE_TYPE_ALIVE = 4
+ENCODING = '!HLLL'
+PADDING = 0
+MESSAGE_TYPE_INIT = 1
+MESSAGE_TYPE_IN = 2
+MESSAGE_TYPE_OUT = 3
+MESSAGE_TYPE_ALIVE = 4
+MESSAGE_TYPE_RESPONSE = 5
+MESSAGE_TYPE_ERROR = 6
 
-    def pack_init_packet(self, lot_id, capacity, vacancies):
-        """
-        packs the init packet as defined by the par king protocol
-        :param lot_id:
-        :param capacity:
-        :param vacancies:
-        :return:
-        """
-        packet = pack(self.ENCODING, self.MESSAGE_TYPE_INIT, lot_id, capacity, vacancies)
-        return packet
 
-    def pack_in_packet(self, lot_id, z_value):
-        """
-        packs the in packet as defined by the par king protocol
-        :param lot_id:
-        :param z_value:
-        :return:
-        """
-        packet = pack(self.ENCODING, self.MESSAGE_TYPE_IN, lot_id, z_value, self.PADDING)
-        return packet
+def pack_init_packet(lot_id, capacity, vacancies):
+    """
+    packs the init packet as defined by the par king protocol
+    :param int lot_id:
+    :param int capacity:
+    :param int vacancies:
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_INIT, lot_id, capacity, vacancies)
+    return packet
 
-    def pack_out_packet(self, lot_id, z_value):
-        """
-        packs the out packet as defined by the par king protocol
-        :param lot_id:
-        :param z_value
-        :return:
-        """
-        packet = pack(self.ENCODING, self.MESSAGE_TYPE_OUT, lot_id, z_value, self.PADDING)
-        return packet
 
-    def pack_alive_packet(self, lot_id):
-        """
-        packs the alive packet as defined by the par king protocol
-        :param lot_id:
-        :return:
-        """
-        packet = pack(self.ENCODING, self.MESSAGE_TYPE_ALIVE, lot_id, self.PADDING, self.PADDING)
-        return packet
+def pack_in_packet(lot_id, z_value):
+    """
+    packs the in packet as defined by the par king protocol
+    :param int lot_id:
+    :param int|float  z_value:
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_IN, lot_id, z_value, PADDING)
+    return packet
 
-    def unpack_packet(self, packet):
-        """
-        unpacks a packet as defined by the par king protocol
-        :param packet:
-        :return: tuple (message_type, lot_id, capacity|padding, vacancies|padding)
-        """
-        packet = unpack(self.ENCODING, packet)
-        return packet
+
+def pack_out_packet(lot_id, z_value):
+    """
+    packs the out packet as defined by the par king protocol
+    :param int lot_id:
+    :param int|float z_value
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_OUT, lot_id, z_value, PADDING)
+    return packet
+
+
+def pack_alive_packet(lot_id):
+    """
+    packs the alive packet as defined by the par king protocol
+    :param int lot_id:
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_ALIVE, lot_id, PADDING, PADDING)
+    return packet
+
+
+def pack_response_packet(available_spaces, error_code=None):
+    """
+    packs the response packet from the server
+    :param int available_spaces:
+    :param int error_code:
+    :return:
+    """
+    packet = pack(ENCODING, MESSAGE_TYPE_RESPONSE, available_spaces, error_code)
+    return packet
+
+
+def unpack_packet(packet):
+    """
+    unpacks a packet as defined by the par king protocol
+    :param packet:
+    :return: tuple (message_type, lot_id, capacity|padding, vacancies|padding)
+    """
+    packet = unpack(ENCODING, packet)
+    return packet
